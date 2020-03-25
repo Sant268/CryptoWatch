90282dbfb835205cd3682912cebea5acf5a1f1b3
Kunal Booch <kunalbooch@hotmail.com>
2020-03-25 16:19:41 +0530

    Moving to Bootstrap

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=INR&api_key=cb4af40ea2f8554c0d0b721c11b328dcc83ddbbd130755ea40c123c778a53487";
-const key = "&api_key=cb4af40ea2f8554c0d0b721c11b328dcc83ddbbd130755ea40c123c778a53487";
-window.onload = function() {document.getElementById("BTC").onclick = function ()
+  document.getElementById("btc").addEventListener("click", getReqBTC);
+ function getReqBTC()
 {
-  const request = new XMLHttpRequest();
-  request.open('GET',url,true);
+  let request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = "text";
+var response = XMLHttpRequest.responseText;
   request.send();
-  request.onload = function() {
-  const json = JSON.parse(request.responseText);
-  document.getElementsByClassName('price')[0].innerHTML = JSON.stringify(json);
-}
-}
+  document.getElementById("price").innerHTML = response['INR'];
 }
