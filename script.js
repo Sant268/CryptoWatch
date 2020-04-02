4ca8673475b65710e514776fc2a709046f1028d1
Kunal Booch <kunalbooch@hotmail.com>
2020-04-02 17:18:16 +0530

    Adding ETH

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=INR&api_key=cb4af40ea2f8554c0d0b721c11b328dcc83ddbbd130755ea40c123c778a53487";
+const url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=INR";
   document.getElementById("btc").addEventListener("click", getReq);
  function getReq()
 {
@@ -6,9 +6,12 @@ const url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=INR&api
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var resultJson = JSON.parse(xhttp.response);
-       document.getElementById("price").innerHTML = resultJson["INR"];
+      console.log(resultJson);
+       document.getElementById("priceB").innerHTML = resultJson["BTC"]["INR"];
+       document.getElementById("priceE").innerHTML = resultJson["ETH"]["INR"];
     }
 };
+
 xhttp.open("GET", url, true);
 xhttp.send();
 }
